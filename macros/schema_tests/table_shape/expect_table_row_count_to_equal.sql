150aa2a81e88722f64f1c601e15c6ac5c526262d
Tao Tang <ttan@habitus.dk>
2025-08-06 16:35:17 +0200

    :sparkles: Feat: Support for actual row count output

--- macros/schema_tests/table_shape/expect_table_row_count_to_equal.sql
+++ macros/schema_tests/table_shape/expect_table_row_count_to_equal.sql
@@ -14,16 +14,44 @@
 
 
 {%- macro default__test_expect_table_row_count_to_equal(model,
-                                                value,
-                                                group_by,
-                                                row_condition
-                                                ) -%}
-{% set expression %}
-count(*) = {{ value }}
-{% endset %}
-{{ dbt_expectations.expression_is_true(model,
-    expression=expression,
-    group_by_columns=group_by,
-    row_condition=row_condition)
-    }}
+                                                        value,
+                                                        group_by,
+                                                        row_condition
+                                                        ) -%}
+
+{%- set where_clause -%}
+    {%- if row_condition -%}
+        where {{ row_condition }}
+    {%- endif -%}
+{%- endset -%}
+
+{%- set group_by_clause -%}
+    {%- if group_by -%}
+        group by {{ group_by | join(", ") }}
+    {%- endif -%}
+{%- endset -%}
+
+with base as (
+    select *
+    from {{ model }}
+    {{ where_clause }}
+),
+
+aggregated as (
+    select
+        count(*) as actual_row_count,
+        {{ value }} as expected_row_count
+    from base
+    {{ group_by_clause }}
+),
+
+failures as (
+    select *
+    from aggregated
+    where actual_row_count != expected_row_count
+)
+
+select *
+from failures
+
 {%- endmacro -%}
